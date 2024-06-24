7fd92add6d8e644c3fa24608fb0a7aef10cc1f1b
jfacina1962 <j.facina@gmail.com>
2024-06-24 12:33:00 -0300

    Troquei a mensagem

--- src/estudos_java/estudos_java.java
+++ src/estudos_java/estudos_java.java
@@ -3,8 +3,7 @@ package estudos_java;
 public class estudos_java {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Neste local se colocam as mensagens
 	}
 
 }
