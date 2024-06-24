a41e191d82410b0374432440e312f6981aca9f2c
jfacina1962 <j.facina@gmail.com>
2024-06-24 14:38:49 -0300

    Programa de gravar salário

--- src/estudos_java/estudos.java
+++ src/estudos_java/estudos.java
@@ -22,7 +22,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class estudos {
+public class estudos  {
     public static void main(String[] args) {
         // Caminho do arquivo
         String nomearquivo = "C:\\JULIO FACINA\\AmbienteJAVA\\nomes_e_enderecos.txt";
@@ -42,3 +42,22 @@ public class estudos {
         }
     }
 }
+
+/*
+    --- RESULTADO DA DIGITAÇÃO ---
+João da Silva, Rua das Flores, 123, salario: 4500
+Maria Oliveira, Avenida Principal, 456, salario: 5000
+Pedro Santos, Travessa das Árvores, 789, salario: 745
+Ana Souza, Praça Central, 321, salario: 3000
+Marcos Costa, Alameda dos Passarinhos, 654, salario: 500
+Carla Pereira, Rua das Palmeiras, 987, salario: 1200
+José Lima, Avenida dos Girassóis, 234, salario: 1000
+Fernanda Almeida, Rua das Margaridas, 567, salario: 2300
+Ricardo Santos, Alameda dos Ipês, 890, salario: 900
+Camila Vieira, Rua das Violetas, 543, salario: 1100
+Felipe Rodrigues, Avenida das Orquídeas, 876, salario: 11000
+Amanda Ferreira, Rua das Acácias, 219, salario: 670
+Lucas Oliveira, Travessa dos Pinheiros, 876, salario: 2200
+Gabriela Silva, Avenida dos Lírios, 543, salario: 1800
+Daniel Costa, Rua das Hortênsias, 210, salario: 1900
+*/
